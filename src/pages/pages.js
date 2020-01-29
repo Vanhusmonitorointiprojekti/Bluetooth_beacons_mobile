f7b71e5cd7f4f81a7a6b6e7db96b1b1052966659
Jani Ristinen <a1800596@myy.haaga-helia.fi>
2020-01-29 11:56:04 +0200

    Vähän tyyliä

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
+
 class AdminFrontPage extends Component {
 
 render() {
         return (
-            <div>
+            <div style={styles.headerStyle}>
                 <h1>Bluetooth beacons admin -page</h1>
-                    <p>info here</p>
+                    <p>Tervetuloa admin käyttöliittymän etusivulle</p>
+                    <p>Tällä sivulta pystyt hallinnoimaan kaikkia laitteita</p>
+                    <button style={styles.buttonstyle}>Test button hello</button>
+                    <button style={styles.buttonstyle}>Test button hello</button>
             </div>
         );
     }
+
+
 }
+
+const styles =  {
+    buttonStyle: {
+        width: 80,
+        height: 80
+    },
+    headerStyle: {
+        textAlign: 'center'
+    }
+        
+} ;
 
 export default AdminFrontPage;
