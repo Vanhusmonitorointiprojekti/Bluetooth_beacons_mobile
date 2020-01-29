7d23fb560a3531a1d70064ffda65daae97f54819
Teemu Sainio <a1800589@myy.haaga-helia.fi>
2020-01-29 15:06:36 +0200

    initial detection code

---
+++ src/detection/detection.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mysql = require('mysql');
+var alert = require('alert-node')
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+//Connection config
+const db = mysql.createConnection({
+  host     : '172.28.170.116',
+  user     : 'connect',
+  password : 'test',
+  database : 'bt_beacons',
+});
+
+//App, listen this port
+app.listen(3000,()=>console.log('Express server is running at port no : 3000'));
+
+// Connect to database
+db.connect((err) => {
+  if(err){
+      throw err;
+  }
+  console.log('Connection established with the database, write /detection to URL to GET');
+});
+
+//GET data
+app.get('/detection',(res,req)=>{
+  
+    function calcSignal() {
+    var signalA = db.query('SELECT beacon_id, signal_db FROM beacon_detections where signal_db > "-50" order by measument_time desc limit 10', (err, rows, fields)=>
+    {
+        console.log(err)
+            console.log('true >', rows)
+    })
+    
+    var signalB = db.query('SELECT beacon_id, signal_db FROM beacon_detections where signal_db < "-50" order by measument_time desc limit 10', (err, rows, fields)=>
+   {
+        console.log(err)
+            console.log('out of range device id:  ' + rows[0].beacon_id, rows)
+        //alert('signal over 50dB')
+    })
+
+}setInterval(calcSignal,10000);
+
+
+});
