3ef8f2850fc68bb9053b602aaf8a9ed847eb69f9
Jani Ristinen <a1800596@myy.haaga-helia.fi>
2020-02-05 11:57:28 +0200

    Renamed admin file to beacon_info

--- src/www/pages/index.js
+++ src/www/pages/index.js
@@ -2,9 +2,9 @@ import React, { Component, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Admin from './admin.js'
 import Beacon_detections from './beacon_detections.js';
 import Receiver_info from './receiver_info.js';
+import Beacon_info from './beacon_info.js';
 
 class AdminFrontPage extends Component{
   constructor(props){
@@ -28,7 +28,7 @@ render(){
            
           </Tabs>
         </AppBar>
-        {this.state.value === 0 && <Admin />}
+        {this.state.value === 0 && <Beacon_info />}
         {this.state.value === 1 && <Beacon_detections />}
         {this.state.value == 2 && <Receiver_info />}
       </div>
