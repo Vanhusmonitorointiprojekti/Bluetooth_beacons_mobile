61ba59835437858b0d3055ff3f16d07bb3744a6a
Jani Maaranen <jani.maaranen@saunalahti.fi>
2020-02-05 13:02:29 +0200

    mqq script created

---
+++ src/detection/mqqt_connect.js
@@ -0,0 +1,56 @@
+var mqtt = require('mqtt');
+var Topic = '#'; //subscribe to all topics
+var Broker_URL = 'mqtt://172.28.170.115';
+
+var options = {
+	clientId: 'beacon-detections',
+	port: 1883,
+	keepalive : 60
+};
+
+var client  = mqtt.connect(Broker_URL, options);
+client.on('connect', mqtt_connect);
+client.on('reconnect', mqtt_reconnect);
+client.on('error', mqtt_error);
+client.on('message', mqtt_messsageReceived);
+client.on('close', mqtt_close);
+
+function mqtt_connect()
+{
+    console.log("Connecting MQTT");
+    client.subscribe(Topic, mqtt_subscribe);
+}
+
+function mqtt_subscribe(err, granted)
+{
+    console.log("Subscribed to " + Topic);
+    if (err) {console.log(err);}
+}
+
+function mqtt_reconnect(err)
+{
+    console.log("Reconnect MQTT");
+    if (err) {console.log(err);}
+	client  = mqtt.connect(Broker_URL, options);
+}
+
+function mqtt_error(err)
+{
+    console.log("Error!");
+	if (err) {console.log(err);}
+}
+
+function after_publish()
+{
+	//do nothing
+}
+
+function mqtt_messsageReceived(topic, message, packet)
+{
+	console.log('Topic=' +  topic + '  Message=' + message);
+}
+
+function mqtt_close()
+{
+	console.log("Close MQTT");
+}
