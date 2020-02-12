23d15593aabfdc2f11db1cc12a14240fc1ce8d8e
Jani Ristinen <a1800596@myy.haaga-helia.fi>
2020-02-12 11:39:36 +0200

    Removed useless react-alerts component

--- src/www/pages/addnew_beacon.js
+++ src/www/pages/addnew_beacon.js
@@ -9,7 +9,7 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { TextField } from '@material-ui/core';
 import axios from 'axios';
-import { Alert } from 'react-alert';
+
 
 
 
