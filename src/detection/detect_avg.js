bd7a86ab1cbc8a4a1d28fd285cb4bf30971777af
Jani Maaranen <jani.maaranen@saunalahti.fi>
2020-02-26 15:46:30 +0200

    all detections work, but might contain errors

--- src/detection/detect_avg.js
+++ src/detection/detect_avg.js
@@ -56,10 +56,147 @@ app.listen(4001,()=>console.log('Service is running at port no : 4001'));
         }setInterval(calcSignal,1000);
     });
 
+    //GET ranneke3 avg detections
+   app.get('/detections/ranneke1',(req,res)=>{
+    function avg_ranneke1() {
 
-    //GET ranneke1 avg detections
+        db.query(
+            '\
+            SELECT( \
+                \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver1" \
+                AND beacon_id = "e2:e3:23:d1:b0:54" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver1_Ranneke1,\
+                \
+                ( \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver2" \
+                AND beacon_id = "e2:e3:23:d1:b0:54" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver2_Ranneke1,\
+                \
+                ( \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver3" \
+                AND beacon_id = "e2:e3:23:d1:b0:54" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver3_Ranneke1 \
+                \
+            ',
+
+            (err, rows, fields)=> {
+
+            if(!err) {
+                console.log(rows)
+            }
+        
+            else {
+              console.log(err)
+            }                
+        })
+        }setInterval(avg_ranneke1, 100);
+});
+
+    //GET ranneke4 avg detections
+    app.get('/detections/ranneke2',(req,res)=>{
+        function avg_ranneke2() {
+
+            db.query(
+                '\
+                SELECT( \
+                    \
+                    SELECT signal_db \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver1" \
+                    AND beacon_id = "d6:2c:ca:c0:d4:9c" \
+                    ORDER BY measument_time DESC \
+                    LIMIT 1 ) AS AVG_Receiver1_Ranneke2,\
+                    \
+                    ( \
+                    SELECT signal_db \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver2" \
+                    AND beacon_id = "d6:2c:ca:c0:d4:9c \
+                    ORDER BY measument_time DESC \
+                    LIMIT 1 ) AS AVG_Receiver2_Ranneke2,\
+                    \
+                    ( \
+                    SELECT signal_db \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver3" \
+                    AND beacon_id = "d6:2c:ca:c0:d4:9c \
+                    ORDER BY measument_time DESC \
+                    LIMIT 1 ) AS AVG_Receiver3_Ranneke2 \
+                    \
+                ',
+
+                (err, rows, fields)=> {
+
+                if(!err) {
+                    console.log(rows)
+                }
+            
+                else {
+                  console.log(err)
+                }                
+            })
+            }setInterval(avg_ranneke2, 1000);
+    });
+
+   //GET ranneke3 avg detections
+   app.get('/detections/ranneke3',(req,res)=>{
+    function avg_ranneke3() {
+
+        db.query(
+            '\
+            SELECT( \
+                \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver1" \
+                AND beacon_id = "f2:36:00:21:c0:50" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver1_Ranneke3,\
+                \
+                ( \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver2" \
+                AND beacon_id = "f2:36:00:21:c0:50" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver2_Ranneke3,\
+                \
+                ( \
+                SELECT signal_db \
+                FROM beacon_detections \
+                WHERE receiver_id = "Receiver3" \
+                AND beacon_id = "f2:36:00:21:c0:50" \
+                ORDER BY measument_time DESC \
+                LIMIT 1 ) AS AVG_Receiver3_Ranneke3\
+                \
+            ',
+
+            (err, rows, fields)=> {
+
+            if(!err) {
+                console.log(rows)
+            }
+        
+            else {
+              console.log(err)
+            }                
+        })
+        }setInterval(avg_ranneke3, 100);
+});
+
+    //GET ranneke4 avg detections
     app.get('/detections/ranneke4',(req,res)=>{
-        function avg_ranneke1() {
+        function avg_ranneke4() {
 
             db.query(
                 '\
@@ -108,5 +245,5 @@ app.listen(4001,()=>console.log('Service is running at port no : 4001'));
                   console.log(err)
                 }                
             })
-            }setInterval(avg_ranneke1, 1000);
+            }setInterval(avg_ranneke4, 1000);
     });
