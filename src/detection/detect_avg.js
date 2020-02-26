ee8519fe6bca9aeb520c9d5f6b1de09049381596
Jani Maaranen <jani.maaranen@saunalahti.fi>
2020-02-26 14:16:06 +0200

    detect wristlet movement working

---
+++ src/detection/detect_avg.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const mysql = require('mysql');
+var alert = require('alert-node');
+const bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(bodyparser.json());
+
+//Connection config
+const db = mysql.createConnection({
+  host     : '172.28.170.116',
+  user     : 'connect',
+  password : 'test',
+  database : 'bt_beacons',
+});
+
+//App, listen this port
+app.listen(4001,()=>console.log('Service is running at port no : 4001'));
+
+// Connect to database
+    db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Connection established with the database');
+    });
+
+    //Write instructions to / -page
+    app.get('/', (req, res) => {
+        function writeInstructions() {
+            res.send(
+            'GET all detections -> /detections/all')
+        }
+        writeInstructions()
+    });
+
+    //GET all detections
+    app.get('/detections/all',(req,res)=>{
+        function calcSignal() {
+
+        db.query(
+            '\
+            SELECT * \
+            FROM beacon_detections where signal_db > "-50" \
+            order by measument_time desc limit 10 \
+            ',
+
+            (err, rows, fields)=>
+        {
+            
+            console.log(err)
+            console.log(rows)
+            
+        })
+        }setInterval(calcSignal,1000);
+    });
+
+
+    //GET ranneke1 avg detections
+    app.get('/detections/ranneke4',(req,res)=>{
+        function avg_ranneke1() {
+
+            db.query(
+                '\
+                SELECT( \
+                    \
+                    SELECT AVG(signal_db) \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver1" \
+                    AND beacon_id = "e2:18:ef:c9:66:f4" \
+                    ORDER BY measument_time DESC \
+                    LIMIT 3 ) AS AVG_Receiver1_Ranneke4,\
+                    \
+                    ( \
+                    SELECT AVG(signal_db) \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver2" \
+                    AND beacon_id = "e2:18:ef:c9:66:f4" \
+                    ORDER BY measument_time DESC \
+                    LIMIT 3 ) AS AVG_Receiver2_Ranneke4,\
+                    \
+                    ( \
+                    SELECT AVG(signal_db) \
+                    FROM beacon_detections \
+                    WHERE receiver_id = "Receiver3" \
+                    AND beacon_id = "e2:18:ef:c9:66:f4" \
+                    ORDER BY measument_time DESC \
+                    LIMIT 3 ) AS AVG_Receiver3_Ranneke4\
+                    \
+                ',
+
+                (err, rows, fields)=> {
+
+                if(!err) {
+                  console.log(rows, "\n AVG values for selected wristband")
+                }
+            
+                else {
+                  console.log(err)
+                }                
+            })
+            }setInterval(avg_ranneke1, 2000);
+    });
