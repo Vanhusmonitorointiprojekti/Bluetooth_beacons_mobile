a12007c011ce8dd1a3c70fb3ab403c76fe9b2b42
Jani Ristinen <a1800596@myy.haaga-helia.fi>
2020-03-11 14:15:01 +0200

    Fixed merge fail that removed edit feature

--- src/www/pages/beacon_info.js
+++ src/www/pages/beacon_info.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Paper, Table, TableRow, TableHead, TableCell, TableBody, Button } from '@material-ui/core';
-import { Link, BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Link, Router, BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import AddBeacon from './addnew_beacon'
+import EditBeacon from "./edit_beacon";
 
 class Beacon_info extends Component {
     constructor(props) {
@@ -9,19 +10,19 @@ class Beacon_info extends Component {
         this.state = {
             tieto: [],
             navigate: false,
-            
+
         }
     }
 
 
     componentDidMount = () => {
         fetch('http://localhost:4000/beacon_info')
-        .then((response) => response.json())
-        .then(responseJson => {
-            this.setState({tieto: responseJson})
-            
-        })
-        }
+            .then((response) => response.json())
+            .then(responseJson => {
+                this.setState({tieto: responseJson})
+
+            })
+    }
 
     handlePress = () => {
         this.setState({navigate: true});
@@ -32,61 +33,63 @@ class Beacon_info extends Component {
 
     delete_beacon = (beacon_id) => {
         fetch('http://localhost:4000/delete/' + beacon_id)
-        .then((response) => response.json())
-        .then((responseJson) =>
-        {
-            this.setState(prevState => ({tieto: prevState.tieto.filter(beacon =>
-                beacon.beacon_id !== beacon_id)
-            }));
-        })
+            .then((response) => response.json())
+            .then((responseJson) =>
+            {
+                this.setState(prevState => ({tieto: prevState.tieto.filter(beacon =>
+                        beacon.beacon_id !== beacon_id)
+                }));
+            })
     }
 
-render() {
+    render() {
         return (
-      <div>
-            <Paper>
-               <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Beacon User</TableCell>
-                            <TableCell>Beacon ID</TableCell>
-                            
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.state.tieto.map(member =>
-                            <TableRow key={member.beacon_id}>
-                            <TableCell>{member.beacon_user}</TableCell>
-                            <TableCell>{member.beacon_id}</TableCell>
-                            <Button onClick={this.delete_beacon.bind(this, member.beacon_id)}>Poista</Button>
-                            </TableRow>
-                            )}
-                            
-                    </TableBody>
-                </Table>
-                </Paper>
-
+            <div>
                 <BrowserRouter>
-                    <div>
-                <nav>
-                <Link to="/AddBeacon"> Add new </Link>
-                
-                </nav>
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Beacon User</TableCell>
+                                    <TableCell>Beacon ID</TableCell>
 
-                <Switch>
-                    <Route path="/AddBeacon">
-                    <AddBeacon />
-                    </Route>
-                </Switch>
-                </div>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.tieto.map(member =>
+                                    <TableRow key={member.beacon_id}>
+                                        <TableCell>{member.beacon_user}</TableCell>
+                                        <TableCell>{member.beacon_id}</TableCell>
+                                        <Button onClick={this.delete_beacon.bind(this, member.beacon_id)}>Poista</Button>
+                                        <Link to={"/beacon/one/"+member.beacon_id}>Muuta</Link>                            </TableRow>
+                                )}
+
+                            </TableBody>
+                        </Table>
+                    </Paper>
+
+
+                    <div>
+                        <nav>
+                            <Link to="/AddBeacon"> Add new </Link>
+
+                        </nav>
+
+                        <Switch>
+                            <Route path="/AddBeacon" component={AddBeacon}></Route>
+                            <Route path="/beacon/one/:id" component={EditBeacon}></Route>
+
+                        </Switch>
+                    </div>
                 </BrowserRouter>
-                </div>
-           
+            </div>
+
         );
-        
+
     }
 
 
 }
+
 
 export default Beacon_info;
