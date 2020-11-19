f3b59028669846337b164ba2e65e0b21525ac53e
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-11-19 20:56:13 +0200

    Changes for my environment

--- App.js
+++ App.js
@@ -17,8 +17,8 @@ import { Text, View, Button, Platform,Linking, } from 'react-native';
 import axios from 'axios';
 
 const Tab = createBottomTabNavigator();
-const PUSH_ENDPOINT = 'http://192.168.1.197:3000/api/push_notification/push_token';
-const PUSH_ENDPOINT2 = "http://192.168.1.197:3000/api/push_notification/message"
+const PUSH_ENDPOINT = 'http://192.168.11.35:4000/api/push_notification/push_token';
+const PUSH_ENDPOINT2 = "http://192.168.11.35:4000/api/push_notification/message"
 
 export default function App() {
   const [expoPushToken, setExpoPushToken] = useState('');
@@ -82,7 +82,7 @@ export default function App() {
     
       }),
     });
-  
+    console.log('post token', token)
     return token;
   }
   return (
--- src/www/pages/push.js
+++ src/www/pages/push.js
@@ -10,8 +10,8 @@ import axios from 'axios';
 
 
 
-const PUSH_ENDPOINT = 'http://192.168.1.197:3000/api/push_notification/push_token';
-const PUSH_ENDPOINT2 = "http://192.168.1.197:3000/api/push_notification/message"
+const PUSH_ENDPOINT = 'http://192.168.11.35:4000/api/push_notification/push_token'
+const PUSH_ENDPOINT2 = "http://192.168.11.35:4000/api/push_notification/message"
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
